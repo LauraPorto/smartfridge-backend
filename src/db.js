8f35d37f7bffc0ab6969cacd57a75d0e175ed19c
Laura <lauraporto596@gmail.com>
2021-05-11 13:10:00 +0200

    .env changed

--- src/db.js
+++ src/db.js
@@ -1,22 +1,10 @@
 const mongoose = require ('mongoose');
 
-const MONGO_CLUSTER = process.env.MONGO_CLUSTER || null;
-const MONGO_SERVER = process.env.MONGO_SERVER || 'mongodb';
-const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
-const MONGO_PORT = process.env.MONGO_PORT || '27017';
-const MONGO_DBNAME = process.env.MONGO_DBNAME || 'mydb';
-const MONGO_USER = process.env.MONGO_USER || null;
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || null;
-
-const QUERY_STRING = MONGO_USER ? 
-`${MONGO_SERVER}://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`:
-`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}`
-
-const db = mongoose.connect('mongodb+srv://admin:1234@cluster0.aeplm.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true});
+const MONGO_URL = process.env.MONGO_URL;
+const QUERY_STRING = `${MONGO_URL}?retryWrites=true&w=majority`;
+const db = mongoose.connect(QUERY_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = db.then((result) => {
     console.log('database connection success');
     return result;
 });
-
-//mongodb+srv://admin:<password>@cluster0.aeplm.mongodb.net/test
