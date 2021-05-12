15d778e8bbcb7f67afb960d9d3be125e6d881b6a
Laura <lauraporto596@gmail.com>
2021-05-12 12:32:32 +0200

    adding cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const express = require("express")
 const app = express()
 const db = require ('./db');
-
+const cors = require('cors');
 //const router = require ('./router');
 
 //app.use(express.json());
+app.use(cors());
 //app.use(router);
 
 // use the express-static middleware
