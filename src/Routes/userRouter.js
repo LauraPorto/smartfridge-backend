60d94a8368aa9cc662633fb1c101d1848c408007
Laura <lauraporto596@gmail.com>
2021-05-12 17:23:59 +0200

    trying to set routers in app.js(export router functions)

--- src/Routes/userRouter.js
+++ src/Routes/userRouter.js
@@ -1,50 +1,50 @@
-//const router = require('express').Router();
+const router = require('express').Router();
 const userController = require('../Controllers/userController');
 const auth = require('../middleware/auth');
 //const adminAuth = require('../Middleware/adminAuth');
 
 //POST
 //Registro de usuarios
-// router.post('/signup', async (req, res) => {
-//     try{
-//         const user = await userController.signUp(req.body);
-//         return res.json(user)
-//     }catch(error){
-//         return res.status(500).json({
-//             message: error.message
-//         });
-//     }
-// });
+router.post('/signup', async (req, res) => {
+    try{
+        const user = await userController.signUp(req.body);
+        return res.json(user)
+    }catch(error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
-// //LogIn 
-// router.post('/login', async (req, res) => {
-//     try{
+//LogIn 
+router.post('/login', async (req, res) => {
+    try{
         
-//         let email = req.body.email;
-//         let password = req.body.password;
+        let email = req.body.email;
+        let password = req.body.password;
 
-//         console.log('Comprobación del post login', email, password);
+        console.log('Comprobación del post login', email, password);
         
-//         let token = await userController.logIn(email, password); 
+        let token = await userController.logIn(email, password); 
         
-//         if(token){
-//             res.json(token);
-//         }else{
-//             res.sendStatus(403);
-//         }
+        if(token){
+            res.json(token);
+        }else{
+            res.sendStatus(403);
+        }
         
-//     }catch(error){
-//         return res.status(500).json({
-//             message: 'Server Error' + error
-//         });
-//     };
-// });
+    }catch(error){
+        return res.status(500).json({
+            message: 'Server Error' + error
+        });
+    };
+});
 
 //LogOut
 
 //GET
 //Todos los usuarios
-export default allUsers = async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const allUsers = await userController.indexAll();
         return res.json(allUsers);
@@ -53,52 +53,52 @@ export default allUsers = async (req, res) => {
             message: error.message
         });
     }
-};
+});
 
-// //Usuario por Id
-// router.get('/:id', auth, async (req, res) => {
-//     try {
-//         let id = req.params.id;
-//         const user = await userController.indexById(id);
-//         return res.json(user);
-//     }catch(error){
-//         return res.status(500).json({
-//             message: error.message
-//         });
-//     }
-// });
+//Usuario por Id
+router.get('/:id', auth, async (req, res) => {
+    try {
+        let id = req.params.id;
+        const user = await userController.indexById(id);
+        return res.json(user);
+    }catch(error){
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
-// //PUT
-// //Modificar los datos del usuario
-// router.put('/:id', async (req, res) => {
+//PUT
+//Modificar los datos del usuario
+router.put('/:id', async (req, res) => {
 
-//     try{
-//         const id = req.params.id;
-//         const user = req.body;
+    try{
+        const id = req.params.id;
+        const user = req.body;
 
-//         const modifiedUser = await userController.update(id, user);
+        const modifiedUser = await userController.update(id, user);
 
-//         return res.status(200).json(modifiedUser);
-//     }catch (error) {
-//         return res.status(500).json({message: error.message
-//         });
-//     }
-// });
+        return res.status(200).json(modifiedUser);
+    }catch (error) {
+        return res.status(500).json({message: error.message
+        });
+    }
+});
 
-// //DELETE
-// //Borrar un usuario 
-// router.delete('/:id', async (req, res) => {
-//     try{
-//         let id = req.params.id;
-//         let result = await userController.delete(id);
-//         let status = 'User deleted';
-//         res.json({status, result});
-//     }catch(error){
-//         res.status(500).json({
-//             message: 'Server Error' + error
-//         });
-//     };
-// });
+//DELETE
+//Borrar un usuario 
+router.delete('/:id', async (req, res) => {
+    try{
+        let id = req.params.id;
+        let result = await userController.delete(id);
+        let status = 'User deleted';
+        res.json({status, result});
+    }catch(error){
+        res.status(500).json({
+            message: 'Server Error' + error
+        });
+    };
+});
 
 
-//module.exports = router;
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -5,29 +5,17 @@ const app = express();
 const db = require ('./db');
 const cors = require('cors');
 
-const userController = require('./Controllers/userController');
-// const storeRouter = require('./Routes/storeRouter');
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static(router));
+app.use(express.static(router));
 // const router = require('express').Router();
 
 
 // use the express-static middleware
 app.use(express.static("public"))
 
-//Todos los usuarios
-app.get('/', async (req, res) => {
-    try {
-        const allUsers = await userController.indexAll();
-        return res.json(allUsers);
-    }catch(error){
-        return res.status(500).json({
-            message: error.message
-        });
-    }
-});
+
 
 // define the first route
 app.get("/", function (req, res) {
