4c3e0c8bade22dcd5a16cb9552d2af6f3889a7a0
Laura <lauraporto596@gmail.com>
2021-05-12 11:47:11 +0200

    changes in procfile and app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("Hello World!")
+})
+
+app.get("/adios", function (req, res) {
+    res.send("Adios World!")
+  })
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
