8302a5dc4f88196e3fa3181d3998062495131712
Laura <lauraporto596@gmail.com>
2021-06-01 22:18:02 +0200

    createServer (deploy)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require("express");
 const router = require('./router');
+const http = require('http');
 const app = express();
 const db = require ('./db');
 const cors = require('cors');
@@ -10,9 +11,12 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
+let server = http.createServer(app);
+
 //arrancamos el servidor
 db.then(() => {
-    app.listen(process.env.PORT || 3001, 
+    server.listen (process.env.PORT || 3001, 
 	() => console.log("Server is running..."));
 }).catch(console.log);
 
+ 
